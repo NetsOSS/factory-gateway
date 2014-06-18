1b9f3bc8172b7a5d33be5d0730a0f1092096b170
Øystein Gammersvik <ogamm@nets.eu>
2014-06-18 13:42:49 +0200

    Commented in GatewayDataJsGenerator

--- gateway-app/src/test/java/eu/nets/factory/gateway/app/MyAppLocalMain.java
+++ gateway-app/src/test/java/eu/nets/factory/gateway/app/MyAppLocalMain.java
@@ -4,7 +4,7 @@ import eu.nets.factory.gateway.web.GatewayDataJsGenerator;
 
 public class MyAppLocalMain {
     public static void main(String[] args) throws Exception {
-        // GatewayDataJsGenerator.main(new String[0]);
+        GatewayDataJsGenerator.main(new String[0]);
         new MyAppMain().run();
     }
 }
