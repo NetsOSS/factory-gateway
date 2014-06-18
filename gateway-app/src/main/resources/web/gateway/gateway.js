e3d36f05dec0e29b25e734c428ba7684bf964f77
ofbje <ofbje@nets.eu>
2014-06-18 09:43:54 +0200

    Added search (in the person example code)

--- gateway-app/src/main/resources/web/gateway/gateway.js
+++ gateway-app/src/main/resources/web/gateway/gateway.js
@@ -16,6 +16,9 @@ define([
   });
 
   gateway.controller('FrontPageCtrl', function ($scope, GatewayData) {
+
+    $scope.hello ="yolo";
+
     GatewayData.PersonController.list().then(function (data) {
       $scope.persons =data;
     });
@@ -23,6 +26,23 @@ define([
        GatewayData.PersonController.create($scope.person);
       console.log("",$scope.person);
 
+    };
+
+    $scope.search = function(){
+      console.log("Search : ",$scope.searchInput);
+      GatewayData.PersonController.search($scope.searchInput).then(function (data) {
+        $scope.searchRes =data;
+      });
+
+    };
+
+
+    // ---------------------- App instance
+    $scope.saveAppInst = function(){
+      //GatewayData.PersonController.create($scope.person);
+      console.log("Saving App Inst : ",$scope.appInst);
+
     }
+
   });
 });
