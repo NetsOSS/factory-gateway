e334cb02c78f268713a6158a32d733940d6292c4
Snorre Lærum <sleru@nets.eu>
2014-06-23 09:55:22 +0200

    Merge branch 'master' of C:\dev\gateway with conflicts.

--- gateway-app/src/main/resources/web/shared/GatewayData.js
+++ gateway-app/src/main/resources/web/shared/GatewayData.js
@@ -128,11 +128,10 @@ define([], function () {
       return ApplicationGroupController.search($route.current.params.name);
     };
     this.ApplicationInstanceController = {};
-    this.ApplicationInstanceController.create = function (applicationId, applicationInstanceModel) {
+    this.ApplicationInstanceController.create = function (applicationInstanceModel) {
       var req = {};
       req.method = 'POST';
-      req.url = prefix + '/data/applications/{applicationId}/instances';
-      req.url = req.url.replace(/{applicationId}/, applicationId);
+      req.url = prefix + '/data/instances';
       req.data = applicationInstanceModel;
       req.params = {};
       return $http(req).then(getData);
