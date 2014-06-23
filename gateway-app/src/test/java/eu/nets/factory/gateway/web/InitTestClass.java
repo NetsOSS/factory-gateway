0b0bbf4e57285ddd47c72f7a4612e795bef8e444
Snorre Lærum <sleru@nets.eu>
2014-06-23 16:40:09 +0200

    Added Initital Test class which creates initial variables. Also added the shell for loadBalancerControllerTest

---
+++ gateway-app/src/test/java/eu/nets/factory/gateway/web/InitTestClass.java
@@ -0,0 +1,66 @@
+package eu.nets.factory.gateway.web;
+
+import eu.nets.factory.gateway.model.Application;
+import eu.nets.factory.gateway.model.ApplicationGroup;
+import eu.nets.factory.gateway.model.ApplicationInstance;
+import eu.nets.factory.gateway.model.LoadBalancer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+import javax.transaction.Transactional;
+
+@Component
+public class InitTestClass {
+
+    @Autowired
+    LoadBalancerController loadBalancerController;
+
+    @Autowired
+    ApplicationController applicationController;
+
+    @Autowired
+    ApplicationGroupController applicationGroupController;
+
+    @Autowired
+    ApplicationInstanceController applicationInstanceController;
+
+    public void init() {
+
+        //LoadBalancers
+        LoadBalancer loadBalancerOne = new LoadBalancer("Per", "hostOne", "instPathOne", "sshOne");
+        LoadBalancer loadBalancerTwo = new LoadBalancer("Knut", "hostOne", "instPathOne", "sshOne");
+        LoadBalancer loadBalancerThree = new LoadBalancer("Hans", "hostOne", "instPathOne", "sshOne");
+        LoadBalancerModel loadModelOne = new LoadBalancerModel(loadBalancerOne);
+        LoadBalancerModel loadModelTwo = new LoadBalancerModel(loadBalancerTwo);
+        LoadBalancerModel loadModelThree = new LoadBalancerModel(loadBalancerThree);
+
+        loadBalancerController.create(loadModelOne);
+        loadBalancerController.create(loadModelTwo);
+        loadBalancerController.create(loadModelThree);
+
+
+        //ApplicationGroups
+        ApplicationGroup groupOne = new ApplicationGroup("GroupOne");
+        ApplicationGroup groupTwo = new ApplicationGroup("GroupTwo");
+        ApplicationGroup groupThree = new ApplicationGroup("GroupThree");
+        AppGroupModel groupModelOne = new AppGroupModel(groupOne);
+        AppGroupModel groupModelTwo = new AppGroupModel(groupTwo);
+        AppGroupModel groupModelThree = new AppGroupModel(groupThree);
+
+        applicationGroupController.create(groupModelOne);
+        applicationGroupController.create(groupModelTwo);
+        applicationGroupController.create(groupModelThree);
+
+        //Applications
+        Application applicationOne = new Application("Kamino", "www.kamino.no", groupOne);
+        Application applicationTwo = new Application("Grandiosa", "www.grandiosa.no", groupTwo);
+        Application applicationThree = new Application("Alpha", "www.alpha.no", groupThree);
+
+        //ApplicationInstances
+        ApplicationInstance appInstOne = new ApplicationInstance("Kamino 1.0", "hostOne", 8080, "www.kamino.no/1.0", applicationOne);
+        ApplicationInstance appInstTwo = new ApplicationInstance("Grandiosa 1.0", "hostTwo", 8080, "www.grandiosa.no/1.0", applicationTwo);
+        ApplicationInstance appInstThree = new ApplicationInstance("Alpha 1.0", "hostThree", 8080, "www.alpha.no/1.0", applicationThree);
+
+    }
+}
---
+++ gateway-app/src/test/java/eu/nets/factory/gateway/web/LoadBalancerControllerTest.java
@@ -0,0 +1,91 @@
+package eu.nets.factory.gateway.web;
+
+import eu.nets.factory.gateway.model.LoadBalancer;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+import eu.nets.factory.gateway.web.LoadBalancerController;
+import eu.nets.factory.gateway.web.WebConfig;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes={WebConfig.class})
+@TransactionConfiguration(defaultRollback = true)
+@WebAppConfiguration
+@ActiveProfiles("unitTest")
+@Transactional
+public class LoadBalancerControllerTest {
+
+    @Autowired
+    private LoadBalancerController loadBalancerController;
+
+    @Autowired
+    private InitTestClass initTestClass;
+
+    @Before
+    public void Before() {
+        initTestClass.init();
+    }
+
+    @Test
+    public void testListAllLoadBalancers() throws Exception {
+        assertNotNull(loadBalancerController);
+    }
+
+    @Test
+    public void testSearch() throws Exception {
+
+    }
+
+    @Test
+    public void testFindById() throws Exception {
+
+    }
+
+    @Test
+    public void testFindBySshKey() throws Exception {
+
+    }
+
+    @Test
+    public void testCreate() throws Exception {
+       LoadBalancer loadBalancer = new LoadBalancer("Snorre", "host", "instpath", "ssH");
+       LoadBalancerModel model = new LoadBalancerModel(loadBalancer);
+       assertEquals(model.name, loadBalancerController.create(model).name);
+       //assertEquals(4, loadBalancerController.listAllLoadBalancers().size());
+    }
+
+    @Test
+    public void testRemove() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+
+    }
+
+    @Test
+    public void testAddApplication() throws Exception {
+
+    }
+
+    @Test
+    public void testGetApplications() throws Exception {
+
+    }
+}
