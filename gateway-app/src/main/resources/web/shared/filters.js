bd9465ea36b535839c96f722c9b8e6a59d8e1edf
Øystein Bjerland <ofbje@nets.eu>
2014-07-08 09:44:12 +0200

    Made a filter for displaying bytes ->MB,KB,etc

--- gateway-app/src/main/resources/web/shared/filters.js
+++ gateway-app/src/main/resources/web/shared/filters.js
@@ -6,7 +6,7 @@ define([ 'angular' ], function (angular) {
 
   filters.filter('secondstime', function () {
     return function (input) {
-      console.log('filter!!!!!!!!!');
+
       var sec_num = parseInt(input, 10) || 0; // don't forget the second param
       if (sec_num == 0)
         return '';
@@ -28,4 +28,14 @@ define([ 'angular' ], function (angular) {
     };
 
   });
+  filters.filter('bytes', function(){
+    return function(bytes){
+      //http://stackoverflow.com/questions/15900485/correct-way-to-convert-size-in-bytes-to-kb-mb-gb-in-javascript
+      if(bytes == 0) return '0 Byte';
+      var k = 1000;
+      var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+      var i = Math.floor(Math.log(bytes) / Math.log(k));
+      return (bytes / Math.pow(k, i)).toPrecision(3) + ' ' + sizes[i];
+    }
+  })
 });
