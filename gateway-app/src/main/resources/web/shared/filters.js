42074c22c025a142a70c8e281231dca25f98a82b
Øystein Bjerland <ofbje@nets.eu>
2014-07-11 11:47:37 +0200

    not showing nan

--- gateway-app/src/main/resources/web/shared/filters.js
+++ gateway-app/src/main/resources/web/shared/filters.js
@@ -30,6 +30,8 @@ define([ 'angular' ], function (angular) {
   });
   filters.filter('bytes', function(){
     return function(bytes){
+      if(isNaN(bytes))
+        return "";
       //http://stackoverflow.com/questions/15900485/correct-way-to-convert-size-in-bytes-to-kb-mb-gb-in-javascript
       if(bytes == 0) return '0 Byte';
       var k = 1000;
