16ec3cf23297ab98e632d3fd51b4f806163a303c
Øystein Bjerland <ofbje@nets.eu>
2014-07-18 09:19:30 +0200

    Fixed a button that didn't work.

--- gateway-app/src/main/resources/web/shared/directives.js
+++ gateway-app/src/main/resources/web/shared/directives.js
@@ -171,6 +171,22 @@ define([ 'angular' ], function (angular) {
     };
   });
 
+  directives.directive( 'goClick', function ( $location ) {
+    return function ( scope, element, attrs ) {
+      var path;
+
+      attrs.$observe( 'goClick', function (val) {
+        path = val;
+      });
+
+      element.bind( 'click', function () {
+        scope.$apply( function () {
+          $location.path( path );
+        });
+      });
+    };
+  });
+
 });
 
 
