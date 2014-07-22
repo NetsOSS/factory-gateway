c3493ec964e3ab8bc2bf4ace325eea58f3a4fc0a
Øystein Gammersvik <ogamm@nets.eu>
2014-07-22 15:36:20 +0200

    FileWriterService - Now catches exceptions on authentication and if the installation path doesn't exist. Throws GatewayExceptions instead.

--- gateway-app/src/main/java/eu/nets/factory/gateway/service/SshConnection.java
+++ gateway-app/src/main/java/eu/nets/factory/gateway/service/SshConnection.java
@@ -1,17 +1,24 @@
 package eu.nets.factory.gateway.service;
 
+import eu.nets.factory.gateway.GatewayException;
 import net.schmizz.sshj.Config;
 import net.schmizz.sshj.DefaultConfig;
 import net.schmizz.sshj.SSHClient;
 import net.schmizz.sshj.connection.channel.direct.Session;
 import net.schmizz.sshj.connection.channel.direct.Session.Command;
 import net.schmizz.sshj.sftp.SFTPClient;
+import net.schmizz.sshj.sftp.SFTPException;
+import net.schmizz.sshj.userauth.UserAuthException;
 import net.schmizz.sshj.xfer.InMemorySourceFile;
+import org.slf4j.Logger;
 
 import java.io.*;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
 public class SshConnection implements Closeable {
 
+    private final Logger log = getLogger(getClass());
 
     private SSHClient sshClient;
 
@@ -21,20 +28,26 @@ public class SshConnection implements Closeable {
         client.addHostKeyVerifier((s, i, publicKey) -> true);
         client.loadKnownHosts(new File("known_hosts"));
         client.connect(host);
-        client.authPublickey(username, client.loadKeys(privateKey, null, null));
-
-//        SSHClient client = new SSHClient(sshConfig);
-//        client.loadKnownHosts(new File("known_hosts"));
-//        client.connect(host);
-//        client.authPublickey("efakt", new File("id_dsa").getAbsolutePath());
-//        return client;
+        try {
+            client.authPublickey(username, client.loadKeys(privateKey, null, null));
+        } catch (UserAuthException e) {
+            String errorMessage = "Could not authenticate " + username + "-user using the \"publickey\" authentication method - " + e.getLocalizedMessage();
+            log.warn(errorMessage, e.getLocalizedMessage());
+            throw new GatewayException(errorMessage);
+        }
         sshClient = client;
     }
 
 
     public void writeRemoteFile(final String fileContents, final String remotePath) throws IOException {
         SFTPClient sftpClient = sshClient.newSFTPClient();
-        sftpClient.put(new InMemoryFile(fileContents), remotePath);
+        try {
+            sftpClient.put(new InMemoryFile(fileContents), remotePath);
+        } catch (SFTPException e) {
+            String errorMessage = "Could not upload file to " + remotePath + ": " + e.getLocalizedMessage() + "/path";
+            log.warn(errorMessage, e);
+            throw new GatewayException(errorMessage);
+        }
     }
 
     public void execute(String ... commands) throws IOException {
