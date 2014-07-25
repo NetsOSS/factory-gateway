bda993cd8fb400780121f05ed1c0c6b2a20105c5
Snorre Lærum <sleru@nets.eu>
2014-07-25 10:50:53 +0200

    Edited configgenerator so that every group has its own port and frontend

--- gateway-app/src/main/java/eu/nets/factory/gateway/service/ConfigGeneratorService.java
+++ gateway-app/src/main/java/eu/nets/factory/gateway/service/ConfigGeneratorService.java
@@ -1,15 +1,16 @@
 package eu.nets.factory.gateway.service;
 
-import eu.nets.factory.gateway.model.Application;
-import eu.nets.factory.gateway.model.ApplicationInstance;
-import eu.nets.factory.gateway.model.HeaderRule;
-import eu.nets.factory.gateway.model.LoadBalancer;
+import eu.nets.factory.gateway.model.*;
+import eu.nets.factory.gateway.web.AppGroupModel;
+import eu.nets.factory.gateway.web.ApplicationGroupController;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
 
 @Service
@@ -20,6 +21,8 @@ public class ConfigGeneratorService {
 
     private final static String TAB = "    ";
     private final static String TAB2 = TAB + TAB;
+    @Autowired
+    ApplicationGroupController applicationGroupController;
 
     public String generateConfig() {
         return null;
@@ -30,15 +33,15 @@ public class ConfigGeneratorService {
      * Sorts the applications. First on name.
      * If 2 applications has the same name, sort on the number of header rules.
      * Such that the app with more headers, will be used before the others which accept all headers.
-     *
+     * <p/>
      * Another way of sorting this, would be to search for all with the same acl path, and negate all other rules.
      * Then the sorting would not be necessary
      */
-    class NameAndRuleComparator implements Comparator<Application>{
+    class NameAndRuleComparator implements Comparator<Application> {
         @Override
         public int compare(Application app1, Application app2) {
             int aclUrl = app1.getPublicUrl().compareTo(app2.getPublicUrl());
-            if(aclUrl==0)
+            if (aclUrl == 0)
                 return app2.getHeaderRules().size() - app1.getHeaderRules().size();
             return aclUrl;
         }
@@ -46,31 +49,45 @@ public class ConfigGeneratorService {
 
     public String generateConfig(LoadBalancer loadBalancer) {
 
-        List<String> rules = new ArrayList<>();
         List<String> backends = new ArrayList<>();
-        List<String> use_backends = new ArrayList<>();
-        int loadBalancerPort = loadBalancer.getStatsPort();
+        HashMap<String, List<String>> frontends = new HashMap<>();
 
         List<Application> lbApplications = loadBalancer.getApplications();
         lbApplications.sort(new NameAndRuleComparator());
 
+
         // Populate variables
-        for (Application application :lbApplications) {
+        for (Application application : lbApplications) {
 
             StringWriter stringWriter = new StringWriter();
             PrintWriter printWriter = new PrintWriter(stringWriter);
 
-            // acl + use_backend
-            //String aclRule = "acl p_" + application.getName() + "_rule path -m beg " + application.getPublicUrl()+" ";
-            StringBuilder aclRules = new StringBuilder();
-            aclRules.append("{ path_beg "+application.getPublicUrl()+" } ");
-            for(HeaderRule headerRule : application.getHeaderRules()){
+            //frontend
+            if(frontends.containsKey(application.getApplicationGroup().getName())) {
+                List<String> values = frontends.remove(application.getApplicationGroup().getName());
+                StringBuilder aclRules = new StringBuilder();
+                aclRules.append("{ path_beg " + application.getPublicUrl() + " } ");
+                for (HeaderRule headerRule : application.getHeaderRules()) {
+                    aclRules.append("{ hdr_reg(" + headerRule.getName() + ") " + headerRule.getPrefixMatch() + " } ");
+                }
 
-                aclRules.append("{ hdr_reg("+headerRule.getName()+") "+headerRule.getPrefixMatch()+" } ");
+                values.add("use_backend " + application.getName() + " if " + aclRules.toString());
+                frontends.put(application.getApplicationGroup().getName(), values);
+
+            } else {
+                List<String> values = new ArrayList<>();
+
+                StringBuilder aclRules = new StringBuilder();
+                aclRules.append("{ path_beg " + application.getPublicUrl() + " } ");
+                for (HeaderRule headerRule : application.getHeaderRules()) {
+
+                    aclRules.append("{ hdr_reg(" + headerRule.getName() + ") " + headerRule.getPrefixMatch() + " } ");
+                }
+
+                values.add("use_backend " + application.getName() + " if " + aclRules.toString());
+                frontends.put(application.getApplicationGroup().getName(), values);
             }
 
-
-            use_backends.add("use_backend " + application.getName() + " if " + aclRules.toString());
 
             // backend
             printWriter.println();
@@ -80,29 +97,22 @@ public class ConfigGeneratorService {
             //debug, adding headers to see which was chosen.
             /// reqadd X-CustomHeader:\ debugMode
             printWriter.println(TAB2 + "reqadd X-ForwardToApp:\\ " + application.getName());
-            //connect timeout
-           /* printWriter.println(TAB2 + "timeout connect " + application.getConnectTimeout() + "ms");
-            //read timeout
-            printWriter.println(TAB2 + "timeout server " + application.getReadTimeout() + "ms");
-            //retry timeout
-            printWriter.println(TAB2 + "retries " + application.getRetryTimeout());*/
-
 
             // Check if app wants sticky cookies.
             if (application.getStickySession().name().equals("STICKY"))
                 printWriter.println(TAB2 + "cookie JSESSIONID prefix");
             if (application.getStickySession().name().equals("STICKY_NEW_COOKIE"))
-                printWriter.println(TAB2 + "cookie SERVERID_"+application.getName()+" insert indirect nocache");
+                printWriter.println(TAB2 + "cookie SERVERID_" + application.getName() + " insert indirect nocache");
 
 //            printWriter.println(TAB2 + "reqrep ^([^\\ :]*)\\ " + application.getPublicUrl() + "/(.*)     \\1\\ /\\2");
 
             // server
-            for(int i = 0; i < application.getApplicationInstances().size(); i++) {
+            for (int i = 0; i < application.getApplicationInstances().size(); i++) {
                 ApplicationInstance applicationInstance = application.getApplicationInstances().get(i);
 
                 String serverConfigLine = TAB2 + "server " + applicationInstance.getName() + " " + applicationInstance.getHost() + ":" + applicationInstance.getPort() + applicationInstance.getPath() + " check";
 
-                if(applicationInstance.getWeight() != 0)
+                if (applicationInstance.getWeight() != 0)
                     serverConfigLine += " weight " + applicationInstance.getWeight();
 
                 if (applicationInstance.getHaProxyState().name().equals("MAINT"))
@@ -122,27 +132,36 @@ public class ConfigGeneratorService {
             //printWriter.flush();
         }
 
-        return buildString(rules, backends, use_backends, loadBalancer);
+        return buildString(frontends, backends, loadBalancer);
     }
 
-    private String buildString(List<String> rules, List<String> backends, List<String> use_backends, LoadBalancer loadBalancer) {
+    private String buildString(HashMap<String ,List<String>> frontends, List<String> backends, LoadBalancer loadBalancer) {
         StringWriter stringWriter = new StringWriter();
         PrintWriter printWriter = new PrintWriter(stringWriter);
-
-        int loadBalancerPort = loadBalancer.getStatsPort();
-
         writeDefaultsStart(printWriter, loadBalancer);
 
-        printWriter.println(TAB2 + "bind *:" + loadBalancer.getStatsPort());
+        //write frontend
+        for(String key: frontends.keySet()) {
+            printWriter.println();
+            printWriter.println(TAB + "frontend " + key);
+            int port = 0;
+            List<AppGroupModel> groups = applicationGroupController.listAllAppGroups();
+            for(AppGroupModel a: groups) {
+                if(a.getName().equals(key)) {
+                    port = a.getPort();
+                    break;
+                }
+            }
+            printWriter.println(TAB2 + "bind *:" + port);
+
+            List<String> values = frontends.get(key);
+            for(String line: values) {
+                printWriter.println(TAB2 + line);
+            }
+        }
 
         // Write content
-        for (String rule : rules) {
-            printWriter.println(TAB2 + rule);
-        }
-        for (String use_backend : use_backends) {
-            printWriter.println(TAB2 + use_backend);
-        }
-        for(String backend : backends) {
+        for (String backend : backends) {
             printWriter.println(TAB2 + backend);
         }
 
@@ -150,7 +169,7 @@ public class ConfigGeneratorService {
         int statsPort = loadBalancer.getStatsPort();
         printWriter.println(TAB + "listen stats *:" + ++statsPort);
 
-        writeDefaultsEnd(printWriter,loadBalancer.getName());
+        writeDefaultsEnd(printWriter, loadBalancer.getName());
 
         //stringWriter.flush();
         //printWriter.flush();
@@ -170,19 +189,14 @@ public class ConfigGeneratorService {
         pw.println(TAB2 + "timeout server " + loadBalancer.getServerTimeout() + "ms");
         pw.println(TAB2 + "timeout client " + loadBalancer.getClientTimeout() + "ms");
         pw.println(TAB2 + "retries " + loadBalancer.getRetries());
-        /*
-        pw.println(TAB2 + "timeout connect 5000ms");
-        pw.println(TAB2 + "timeout client 50000ms");
-        pw.println(TAB2 + "timeout server 50000ms");*/
-        pw.println();
-        pw.println(TAB + "frontend http-in");
+
     }
 
-    private void writeDefaultsEnd(PrintWriter pw,String nodeName) {
+    private void writeDefaultsEnd(PrintWriter pw, String nodeName) {
         pw.println(TAB2 + "mode http");
         pw.println(TAB2 + "stats enable");
         pw.println(TAB2 + "stats uri /proxy-stats");
         pw.println(TAB2 + "stats admin if TRUE");
-        pw.println(TAB2 + "stats show-node "+nodeName);
+        pw.println(TAB2 + "stats show-node " + nodeName);
     }
 }
